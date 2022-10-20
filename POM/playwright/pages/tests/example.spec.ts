c363dad306369131ced11e59b3b8f94ba4bd77d0
Nikita Kirpichoev <nalkire17@gmail.com>
2022-10-20 15:52:09 +0300

    playwright test

--- POM/playwright/pages/tests/example.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-import { LoginPage } from '../login-page';
-
-test('Login test', async ({ page }) => {
-  const loginPage = new LoginPage(page);
-  await loginPage.visit();
-  await new LoginPage(page).login('username', 'pass');
-  
-});
