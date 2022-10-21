ec3d3ce03f39c05422a6499d5d411a158ba6b6a7
Nikita Kirpichoev <nalkire17@gmail.com>
2022-10-21 19:10:32 +0300

    cypress tests

---
+++ POM/cypress/e2e/login.spec.cy.js
@@ -0,0 +1,27 @@
+import Login from "../integration/page-objects/login";
+import * as loginData from "../fixtures/login.mock.json";
+
+describe("Login", function () {
+  const login = new Login ();
+
+  it('succses', () => {
+    cy.visit('https://the-internet.herokuapp.com/login');
+    login.getUserName().type(loginData.username);
+    login.getPassword().type(loginData.password);
+    login.loginButton().click();
+    cy.get('#flash')
+          .should('exist')
+          .contains('You logged into a secure area!');
+  })
+
+  it('failed (wrong password)', () => {
+    cy.visit('https://the-internet.herokuapp.com/login');
+    login.getUserName().type(loginData.username);
+    login.getPassword().type(loginData.passwordWrong);
+    login.loginButton().click();
+    cy.get('#flash')
+          .should('exist')
+          .contains('Your password is invalid!');
+  })
+
+})
